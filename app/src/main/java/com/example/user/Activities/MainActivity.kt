15c33473e4cc46473c1881c83d7351352d0d9782
AllenCHANcyh <allencyh2001@gmail.com>
2022-12-05 14:13:07 +0800

    share budget to Jason

--- app/src/main/java/com/example/user/Activities/MainActivity.kt
+++ app/src/main/java/com/example/user/Activities/MainActivity.kt
@@ -12,12 +12,12 @@ import java.io.File
 
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var btn: Button;
+//
+//    lateinit var btn: Button;
     lateinit var btn2: Button;
-    lateinit var btn3: Button;
-    lateinit var btn4: Button
-    lateinit var hello: TextView;
+//    lateinit var btn3: Button;
+//    lateinit var btn4: Button
+//    lateinit var hello: TextView;
     private lateinit var masterDataDriver: DataDriver;
 
 
@@ -29,48 +29,49 @@ class MainActivity : AppCompatActivity() {
         var fileDir: File = filesDir;
         this.masterDataDriver = DataDriver(fileDir);
 
-        btn4 = findViewById(R.id.button4);
-        btn4.setOnClickListener{
-            masterDataDriver.createPassword("AllenCHAN")
-            println(masterDataDriver.getCurrentUser().getPassword());
-        };
-
-
-        btn2 = findViewById(R.id.button2)
-        btn2.setOnClickListener{
+//
+//        btn4 = findViewById(R.id.button4);
+//        btn4.setOnClickListener{
 //            masterDataDriver.createPassword("AllenCHAN")
-            startActivity(Intent(
-                this, BudgetDetails::class.java)
-                .putExtra("data",masterDataDriver)
-            )
-        };
-
-
-        hello = findViewById(R.id.textView);
-        btn3 = findViewById(R.id.button3)
-        btn3.setOnClickListener{
-            masterDataDriver.getCurrentBudgetItem().setBudgetAmount(100.0);
-            hello.setText(masterDataDriver.getCurrentBudgetItem().getBudgetAmount().toString());
-            masterDataDriver.createPassword("Addoil20010717");
-            println(masterDataDriver.getPassword());
-            masterDataDriver.createNewCategory("Other", "", "");
-            masterDataDriver.createNewExpense("Testing", "Other", 100.0,1,
-            "","","","",false);
-            println(masterDataDriver.getCategoryList().get(0));
-            println(masterDataDriver.getExpenseItemList().get(0));
-        }
-
-
-
-
-        btn = findViewById(R.id.button)
-        btn.setOnClickListener{
-            startActivity(
-                Intent(
-                    this, MainActivity2::class.java)
-                    .putExtra("data",masterDataDriver)
-            )
-        }
+//            println(masterDataDriver.getCurrentUser().getPassword());
+//        };
+//
+//
+//        btn2 = findViewById(R.id.)
+//        btn2.setOnClickListener{
+////            masterDataDriver.createPassword("AllenCHAN")
+//            startActivity(Intent(
+//                this, BudgetDetails::class.java)
+//                .putExtra("data",masterDataDriver)
+//            )
+//        };
+//
+//
+//        hello = findViewById(R.id.textView);
+//        btn3 = findViewById(R.id.button3)
+//        btn3.setOnClickListener{
+//            masterDataDriver.getCurrentBudgetItem().setBudgetAmount(100.0);
+//            hello.setText(masterDataDriver.getCurrentBudgetItem().getBudgetAmount().toString());
+//            masterDataDriver.createPassword("Addoil20010717");
+//            println(masterDataDriver.getPassword());
+//            masterDataDriver.createNewCategory("Other", "", "");
+//            masterDataDriver.createNewExpense("Testing", "Other", 100.0,1,
+//            "","","","",false);
+//            println(masterDataDriver.getCategoryList().get(0));
+//            println(masterDataDriver.getExpenseItemList().get(0));
+//        }
+//
+//
+//
+//
+//        btn = findViewById(R.id.button)
+//        btn.setOnClickListener{
+//            startActivity(
+//                Intent(
+//                    this, MainActivity2::class.java)
+//                    .putExtra("data",masterDataDriver)
+//            )
+//        }
 
 
     }
